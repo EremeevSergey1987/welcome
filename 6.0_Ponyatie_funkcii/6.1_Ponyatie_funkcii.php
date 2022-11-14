df50faefca9b57183b69ea01be4f2418f7e61fcd
Sergey Eremeev <eremeev87@bk.ru>
2022-11-14 17:11:49 +0800

    'message'

--- 6.0_Ponyatie_funkcii/6.1_Ponyatie_funkcii.php
+++ 6.0_Ponyatie_funkcii/6.1_Ponyatie_funkcii.php
@@ -1,11 +1,34 @@
 <?php
+
+$supportedOperator = ["+", "-", "*"];
+
 function calculeteOperation (int $a, int $b, string $operation = "-") : int
 {
     if($operation == "+"){
         return $a + $b;
     }elseif ($operation == "-"){
         return $a - $b;
+    }elseif ($operation == "*"){
+        return $a * $b;
     }
 
 }
-echo calculeteOperation(2, 5);
+
+function pasceOperator($userInput, $operator){
+    $parceResult = explode($userInput, $operator);
+    if($parceResult && count($parceResult) == 2 ){
+        return ['operators' => $parceResult, 'operator' => $operator];
+    }
+    return false;
+}
+
+do{
+   $userInput = readline("Введите выражение: ");
+   foreach ($supportedOperator as $operator){
+       $parceResult = pasceOperator($operator, $userInput);
+       if($parceResult){
+           echo "Результат - " . calculeteOperation(intval($parceResult['operators'][0]), intval($parceResult['operators'][1]), $parceResult['operator']) . PHP_EOL;
+       }
+   }
+}
+while(true);
--- 7.1_Rekursivnyj_vyzov_funkcij/7.6_Prakticheskaya_rabota/index.php
+++ 7.1_Rekursivnyj_vyzov_funkcij/7.6_Prakticheskaya_rabota/index.php
@@ -3,19 +3,23 @@ $searchRoot = 'first_dir';
 $searchName = 'test.txt';
 $searchResult = [];
 
-function serchFile($searchRoot, $searchName, &$searchResult){
-    $listElementDir = scandir('first_dir');
-    print_r($listElementDir);
-    var_dump(is_dir($listElementDir[0]));
-}
+//function serchFile($searchRoot, $searchName, &$searchResult){
+//    $listElementDir = scandir('first_dir');
+//    print_r($listElementDir);
+//    var_dump(is_dir($listElementDir[0]));
+//}
 
 $listElementDir = scandir('first_dir\.');
 print_r($listElementDir);
-var_dump(is_dir($listElementDir[1]));
 
-if($listElementDir[2] === $searchName){
-    echo 'Файл найден!';
-}
-else{
-    echo 'Файл не найден!';
-}
+
+var_dump(is_dir('first_dir'. DIRECTORY_SEPARATOR . $listElementDir[6]));
+var_dump(is_dir('first_dir'. DIRECTORY_SEPARATOR . $listElementDir[7]));
+echo DIRECTORY_SEPARATOR;
+var_dump(DIRECTORY_SEPARATOR);
+//if($listElementDir[2] === $searchName){
+//    echo 'Файл найден!';
+//}
+//else{
+//    echo 'Файл не найден!';
+//}
