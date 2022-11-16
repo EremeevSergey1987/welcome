7797ab4cf39b0f8c570c00df32cf3feadcf8d474
Sergey Eremeev <eremeev87@bk.ru>
2022-11-16 17:19:44 +0800

    'recursion'

--- 7.1_Rekursivnyj_vyzov_funkcij/7.6_Prakticheskaya_rabota/index.php
+++ 7.1_Rekursivnyj_vyzov_funkcij/7.6_Prakticheskaya_rabota/index.php
@@ -5,25 +5,25 @@ $searchResult = [];
 
 function serchFile(string $searchRoot, string $searchName, &$searchResult)
 {
-
     $listElementsDir = scandir($searchRoot);
-    $count = count($listElementsDir);
-    for($i = 2; $i < $count; $i++){
+    for($i = 2; $i < count($listElementsDir); $i++){
         $is_dir = is_dir($searchRoot . DIRECTORY_SEPARATOR . $listElementsDir[$i]);
-
         if($is_dir){
             serchFile($searchRoot . DIRECTORY_SEPARATOR . $listElementsDir[$i], $searchName, $searchResult);
         }
-
-        if($listElementsDir[$i] == $searchName){
-            $searchResult[] = $searchRoot . DIRECTORY_SEPARATOR . $searchName;
-            return $searchResult;
-        }
         else{
-
+            if($listElementsDir[$i] == $searchName){
+                if(filesize($searchRoot . DIRECTORY_SEPARATOR . $searchName))
+                {
+                    $searchResult[] = $searchRoot . DIRECTORY_SEPARATOR . $searchName . " - " . filesize($searchRoot . DIRECTORY_SEPARATOR . $searchName);
+                }
+            }
+            else{
+                return "Поиск не дал результатов!";
+            }
         }
-
     }
+    return $searchResult;
 }
 $result = serchFile($searchRoot, $searchName, $searchResult);
 print_r($result);
