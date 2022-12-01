655fc2db01fee2221dbeb692f930b279f2638c96
Sergey Eremeev <eremeev87@bk.ru>
2022-12-01 16:00:16 +0800

    'work_10'

---
+++ 10_Interfejsy/10.2_Polimorfizm.php
@@ -0,0 +1,57 @@
+<?php
+
+interface Animal {
+    public function makeNoise();
+    public function makeMotion();
+}
+
+class Bird implements Animal
+{
+    public function makeNoise()
+    {
+        echo 'Кукареку!' . PHP_EOL;
+    }
+    public function makeMotion()
+    {
+        echo 'Я лечу!' . PHP_EOL;
+    }
+}
+
+class Mammal implements Animal {
+    public function makeNoise()
+    {
+        echo 'М-у-у-у...' . PHP_EOL;
+    }
+    public function makeMotion()
+    {
+        echo 'Медленно иду' . PHP_EOL;
+    }
+}
+
+class Ferm {
+    private $animal;
+    public function addAnimal(Animal $animal){
+        $this->animals[] = $animal;
+    }
+    public function showAnimals() {
+        foreach ($this->animals as $animal){
+            $animal->makeNoise();
+            $animal->makeMotion();
+
+        }
+    }
+}
+
+$cow = new Mammal();
+$dog = new Mammal();
+$penguin = new Bird();
+$rooster = new Bird();
+$farm = new Ferm();
+
+$farm->addAnimal($cow);
+
+//$farm->addAnimal($dog);
+//$farm->addAnimal($penguin);
+//$farm->addAnimal($rooster);
+
+$farm->showAnimals();
