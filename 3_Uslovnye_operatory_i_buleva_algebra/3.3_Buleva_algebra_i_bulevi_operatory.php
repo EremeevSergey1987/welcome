d66e66974176ee2606cbe1b4e24771c2757569ec
Sergey Eremeev <eremeev87@bk.ru>
2022-12-08 16:14:42 +0800

    'work_10'

--- 3_Uslovnye_operatory_i_buleva_algebra/3.3_Buleva_algebra_i_bulevi_operatory.php
+++
@@ -1,9 +0,0 @@
-<?php
-$a = false;
-$b = false;
-$c = true;
-
-$d = $a || ! ($b && $c) ? 1 : 0;
-var_dump($d);
-
-
--- 6.0_Ponyatie_funkcii/6.2_Parametry_funkcii.php
+++
@@ -1,6 +0,0 @@
-<?php
-function displayParametrs ($a, $b, $c = 0){
-    echo $a . ' ' . $b . ' ' . $c . PHP_EOL;
-}
-
-displayParametrs(1,2,3);
