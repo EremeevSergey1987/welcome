a062d0fcd73848247eea1da2397035ec04c5f50d
Sergey Eremeev <eremeev87@bk.ru>
2022-12-19 23:34:36 +0800

    'dz16'

--- 16_Obrabotka_oshibok_Exceptions_i_logi/16.6_Practical_work/input_text.php
+++ 16_Obrabotka_oshibok_Exceptions_i_logi/16.6_Practical_work/input_text.php
@@ -1,18 +1,13 @@
 <?php
 require_once 'autoload.php';
-use ClassTelegraphText as NN;
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <title>Form</title>
 </head>
@@ -26,15 +21,11 @@ use ClassTelegraphText as NN;
                     if(strlen($_POST['author']) > 0 && strlen($_POST['email']) > 0 && strlen($_POST['text']) <= 500){
                         var_dump($_POST) . PHP_EOL;
                         $objFileStorage = new FileStorage();
-                        $objTelegraphText = new NN\TelegraphText('test_text_file.txt');
-
+                        $objTelegraphText = new TelegraphText('test_text_file.txt');
                         $objTelegraphText->author = $_POST['author'];
                         $objTelegraphText->published = date("Y-m-d");
                         $objTelegraphText->text = $_POST['text'];
-
-                        //--------- создание -------------
                         $slugReturn = $objFileStorage->create($objTelegraphText);
-                        //--------- создание -------------
                         if($slugReturn){
                             echo '<div class="alert alert-success" role="alert">Данные успешно отправлены!</div>';
                         }
@@ -45,27 +36,24 @@ use ClassTelegraphText as NN;
                 }
                 ?>
 
-
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Author</label>
-                    <input class="form-control" id="author" name="author" type="text" placeholder="Author" aria-label="Author">
+                    <input class="form-control" id="author" name="author" value="<?php if($_POST){ echo $_POST['author'];}?>" type="text" placeholder="Author" aria-label="Author">
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="name@mail.ru" aria-describedby="emailHelp">
+                    <input type="email" value="<?php if($_POST){ echo $_POST['email'];}?>" class="form-control" id="email" name="email" placeholder="name@mail.ru" aria-describedby="emailHelp">
                     <div id="emailHelp" class="form-text">We will never share your email with anyone else. But this is not accurate</div>
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlTextarea1" class="form-label">Text</label>
-                    <textarea class="form-control" id="text" name="text" rows="3"></textarea>
+                    <textarea class="form-control" id="text" name="text" rows="3"><?php if($_POST){ echo $_POST['text'];}?></textarea>
                 </div>
 
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-
-
 
         </div>
     </div>
